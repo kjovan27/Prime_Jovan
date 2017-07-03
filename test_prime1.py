58b06013f924ee86e5534993f79878912660f442
Jovan Kizito <kjovan27@gmail.com>
2017-07-03 06:34:03 -0700

    Update test_prime1.py

--- test_prime1.py
+++ test_prime1.py
@@ -4,7 +4,17 @@ from prime import is_prime
 class PrimeTestCase(unittest.TestCase):
 
     def test_is_zero_prime(self):
-        self.assertTrue(is_prime(0))
+        self.assertFalse(is_prime(0))
+        
+    def test_is_five_prime(self):
+        self.assertTrue(is_prime(5))
+        
+    def test_negative_number(self):
+        self.assertFalse(is_prime(-1))
+        self.assertFalse(is_prime(-2))
+        self.assertFalse(is_prime(-3))
+  #  def test_isNot_string(self):
+  #      self.assertFalse(is_prime())
 
 if __name__ == '__main__':
     unittest.main()
