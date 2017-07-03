624ec5031083ca3d9703e78a3584217f7532a7fe
Jovan Kizito <kjovan27@gmail.com>
2017-07-03 06:25:59 -0700

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,4 +1,8 @@
 def is_prime(number):
+    if number <= 0:
+        return False
+    if number in (0, 1):
+        return False
     for num in range(2, number):
         if number % num == 0:
             return False
